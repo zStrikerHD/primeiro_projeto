c28ca9fd718be9d17917c73055383bfa157f487b
Giovani <giovani@teste.com>
2025-02-05 19:28:37 -0300

    Correção das rotas e configuração do Vercel

--- api/auth.js
+++ api/auth.js
@@ -1,33 +1,4 @@
-import axios from "axios";
-import dotenv from "dotenv";
-
-dotenv.config();
-
-export default async function handler(req, res) {
-    const { code } = req.query;
-
-    if (!code) {
-        return res.status(400).json({ error: "Código de autorização não encontrado!" });
-    }
-
-    try {
-        const response = await axios.post(
-            "https://www.bling.com.br/Api/v3/oauth/token",
-            new URLSearchParams({
-                grant_type: "authorization_code",
-                code: code,
-                redirect_uri: process.env.REDIRECT_URI
-            }),
-            {
-                headers: {
-                    "Content-Type": "application/x-www-form-urlencoded",
-                    "Authorization": "Basic " + Buffer.from(`${process.env.CLIENT_ID}:${process.env.CLIENT_SECRET}`).toString("base64")
-                }
-            }
-        );
-
-        return res.json(response.data);
-    } catch (error) {
-        return res.status(500).json({ error: error.response?.data || error.message });
-    }
-}
+module.exports = (req, res) => {
+    const authUrl = `https://www.bling.com.br/Api/v3/oauth/authorize?response_type=code&client_id=${process.env.CLIENT_ID}&redirect_uri=${process.env.REDIRECT_URI}&state=12345`;
+    res.redirect(authUrl);
+};
--- api/callback.js
+++ api/callback.js
@@ -1,30 +1,26 @@
-require('dotenv').config();
-const axios = require('axios');
+const axios = require("axios");
 
 module.exports = async (req, res) => {
-    if (req.method !== 'GET') {
-        return res.status(405).json({ error: "Method Not Allowed" });
-    }
-
     const authCode = req.query.code;
+
     if (!authCode) {
         return res.status(400).json({ error: "Código de autorização não encontrado!" });
     }
 
     try {
-        const response = await axios.post('https://www.bling.com.br/Api/v3/oauth/token', new URLSearchParams({
-            grant_type: 'authorization_code',
+        const response = await axios.post("https://www.bling.com.br/Api/v3/oauth/token", new URLSearchParams({
+            grant_type: "authorization_code",
             code: authCode,
             redirect_uri: process.env.REDIRECT_URI
         }), {
             headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-                'Authorization': 'Basic ' + Buffer.from(`${process.env.CLIENT_ID}:${process.env.CLIENT_SECRET}`).toString('base64')
+                "Content-Type": "application/x-www-form-urlencoded",
+                "Authorization": "Basic " + Buffer.from(`${process.env.CLIENT_ID}:${process.env.CLIENT_SECRET}`).toString("base64")
             }
         });
 
-        return res.json(response.data);
+        res.json(response.data);
     } catch (error) {
-        return res.status(500).json({ error: error.response?.data || error.message });
+        res.status(500).json({ error: error.response?.data || error.message });
     }
 };
